5612c17f66079079ce25947d858550bd8a61d4d3
张瀚 <10772773+Z4484785@user.noreply.gitee.com>
2022-11-15 17:16:49 +0800

    second

---
+++ cmake-build-debug/CMakeFiles/robot_v3.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robot_v3.dir/main.cpp.obj"
+  "CMakeFiles/robot_v3.dir/main.cpp.obj.d"
+  "CMakeFiles/robot_v3.dir/robot.cpp.obj"
+  "CMakeFiles/robot_v3.dir/robot.cpp.obj.d"
+  "librobot_v3.dll.a"
+  "robot_v3.exe"
+  "robot_v3.exe.manifest"
+  "robot_v3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robot_v3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/robot_v3.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for robot_v3.
+# This may be replaced when dependencies are built.
